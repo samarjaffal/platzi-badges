cb0df804859078055f51ab374cb6a4d7ae4bd8fd
Samar Jaffal <samarjaffalh@gmail.com>
2020-04-02 21:57:42 -0400

    Updates

--- README.md
+++
@@ -1,9 +0,0 @@
-# Platzi Badges
-Platzi Badges is a React Web App for:  React.js Platzi Course. This platform allow the user create, edit, delete and list all the guests for a new Platzi Conf.
-
-# Features
-- React Router
-- Single Page Application (SPA)
-- React Hooks
-
-![Home - Platzi Badges](src/images/platzi_badges.png)
---
+++ precache-manifest.4840f34adaa78e2d252db0fd98740cee.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed7c58eafe17eff3b2557c0369b20264",
+    "url": "/platzi-badges/static/media/badge-header.ed7c58ea.svg"
+  },
+  {
+    "revision": "b5a529f2e48b1fd78ed3",
+    "url": "/platzi-badges/static/css/main.777ae3c8.chunk.css"
+  },
+  {
+    "revision": "0f7117b146aebd482afe",
+    "url": "/platzi-badges/static/js/runtime~main.0f7117b1.js"
+  },
+  {
+    "revision": "6841730f1bbe3235e7a0cdc7d244d244",
+    "url": "/platzi-badges/static/media/logo.6841730f.svg"
+  },
+  {
+    "revision": "2aa0eef622711bdc9255",
+    "url": "/platzi-badges/static/js/2.2aa0eef6.chunk.js"
+  },
+  {
+    "revision": "a1880fbfb5634577b88090c9f82b3de7",
+    "url": "/platzi-badges/static/media/platziconf-logo.a1880fbf.svg"
+  },
+  {
+    "revision": "b5a529f2e48b1fd78ed3",
+    "url": "/platzi-badges/static/js/main.b5a529f2.chunk.js"
+  },
+  {
+    "revision": "0abcdfcd1e323649a0338a204fe8b853",
+    "url": "/platzi-badges/static/media/stars.0abcdfcd.svg"
+  },
+  {
+    "revision": "f4d2007fd3912c51d17a1e29df28d115",
+    "url": "/platzi-badges/static/media/404.f4d2007f.png"
+  },
+  {
+    "revision": "0d2ff286deb3895d3ee82fed78c4dce4",
+    "url": "/platzi-badges/static/media/astronauts.0d2ff286.svg"
+  },
+  {
+    "revision": "2aa0eef622711bdc9255",
+    "url": "/platzi-badges/static/css/2.ca3280c9.chunk.css"
+  },
+  {
+    "revision": "e10169726cb5eb2e404f34eae00789ad",
+    "url": "/platzi-badges/index.html"
+  }
+];
